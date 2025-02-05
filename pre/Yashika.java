4c7364bc9a4c3eabe8bf177364f6dc371c278453
yaashikasaini <122611383+yaashikasaini@users.noreply.github.com>
2025-02-05 14:51:31 +0530

    Add files via upload

---
+++ pre/Yashika.java
@@ -0,0 +1,12 @@
+public class Yashika{
+public static void main(String [] args){
+System.out.println(" she is running");
+}
+public static void main1(Integer [] args){
+System.out.println(" she is sleeping ");
+}
+public static void main(Character [] args){
+    System.out.println("2+2"+ 4*2);
+
+}
+}
