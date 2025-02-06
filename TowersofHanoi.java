8e6a3aab355d592a5ef7e8a8c6ea2f72335d0d61
yaashikasaini <122611383+yaashikasaini@users.noreply.github.com>
2025-02-06 10:32:11 +0530

    Add files via upload

---
+++ TowersofHanoi.java
@@ -0,0 +1,16 @@
+public class TowersofHanoi {
+    public static void solve(int n,char source, char rod, char desti){
+        if(n==1){
+            System.out.println("Move disk 1 from "+source+" to"+desti );
+            return;
+        }
+        solve(n-1,source,desti,rod);
+        System.out.println("Move disk "+n+" from "+source+" to "+desti);
+        solve(n-1,rod,source,desti);
+
+    }
+    public static void main(String[] args) {
+        int m=4;
+        solve(m, 'A', 'B', 'C');
+    }
+}
